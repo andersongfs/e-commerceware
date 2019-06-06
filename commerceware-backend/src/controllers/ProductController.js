992b514b96c452ffb098ca49f702e75d9400911e
Anderson Gustafson <andersongfs@gmail.com>
2019-06-06 17:12:15 -0300

    crud products

--- commerceware-backend/src/controllers/ProductController.js
+++ commerceware-backend/src/controllers/ProductController.js
@@ -1,6 +1,16 @@
 const Product = require('../models/Product')
 
 class ProductController {
+  async index (req, res) {
+    const product = await Product.find()
+    return res.json(product)
+  }
+
+  async show (req, res) {
+    const product = await Product.findById(req.params.id)
+    return res.json(product)
+  }
+
   async store (req, res) {
     const { title } = req.body
 
@@ -10,6 +20,18 @@ class ProductController {
 
     const product = await Product.create(req.body)
     return res.json(product)
+  }
+
+  async update (req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    return res.json(product)
+  }
+
+  async destroy (req, res) {
+    await Product.findByIdAndDelete(req.params.id)
+    return res.send()
   }
 }
 module.exports = new ProductController()
--- commerceware-backend/src/routes/routes.js
+++ commerceware-backend/src/routes/routes.js
@@ -7,6 +7,10 @@ const controllers = require('../controllers')
 /**
  * Product routes
  */
+routes.get('/products', controllers.ProductController.index)
+routes.get('/products/:id', controllers.ProductController.show)
 routes.post('/products', controllers.ProductController.store)
+routes.put('/products/:id', controllers.ProductController.update)
+routes.delete('/products/:id', controllers.ProductController.destroy)
 
 module.exports = routes
