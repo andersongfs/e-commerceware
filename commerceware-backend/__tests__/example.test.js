6c77f067080c611beb311b218f11976f1c9fce83
Anderson Gustafson <andersongfs@gmail.com>
2019-06-10 19:05:23 -0300

    First test

--- commerceware-backend/__tests__/example.test.js
+++ commerceware-backend/__tests__/example.test.js
@@ -1,17 +1,20 @@
 const request = require('supertest')
+const SERVER = 'http://localhost:3333'
 
-const server = require('../server')
-
-describe('Product-Route', () => {
+describe('GET /products', () => {
   it('should be able to create a new product with valid properties', async () => {
-    const response = await request(server)
+    const product = { title: 'Product jest9', price: 50, promotion: 0 }
+    const response = await request(SERVER)
       .post('/products')
       .send({
-        title: 'Product jest',
-        price: 50,
-        promotion: 0
+        title: product.title,
+        price: product.price,
+        promotion: product.promotion
       })
-
+    console.log(response.text.\"title\")
     expect(response.status).toBe(200)
+    expect(response.text).toHaveProperty('title', product.title)
+    expect(response.text).toHaveProperty('price', product.price)
+    expect(response.text).toHaveProperty('promotion', product.promotion)
   })
 })
--- commerceware-backend/server.js
+++ commerceware-backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv')
+require('dotenv').config
 const express = require('express')
 const mongoose = require('mongoose')
 const validate = require('express-validation')
@@ -8,7 +8,6 @@ class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
-
     this.database()
     this.middlewares()
     this.routes()
